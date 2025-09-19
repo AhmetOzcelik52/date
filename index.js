c0cac148cb3c94de2f48ca0a37d73ee911a37995
AhmetOzcelik52 <ozcelikahmet165@gmail.com>
2025-09-19 17:53:38 +0200

    1st commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ document.getElementById('DateFinder').addEventListener('click', function() {
             var DateStr = today.toLocaleDateString('tr-TR');
             document.getElementById('ShowDate').textContent = DateStr;
 });  
-
+ 
  document.getElementById('DateClear').addEventListener('click', function() {
             document.getElementById('ShowDate').textContent = ''; 
-        });     
+        });      
